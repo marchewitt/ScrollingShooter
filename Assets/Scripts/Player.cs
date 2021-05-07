a319ed18ad50678f1aa2fedd23373f9f822ff0c1
Marc Hewitt <marc@marchewitt.com>
2021-05-07 17:47:21 -0500

    Speed PowerUp Implemented
    Needs to be added to spawnmanager still but it functions. Had to rework speed to baseSpeed and add a _speed to multiply.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private int health = 3;
     [Tooltip("Players movement speed")]
-    [SerializeField] private float speed = 3.5f;
+    [SerializeField] private float baseSpeed = 3.5f;
+    private float _speed = 0;
 
     [Header("Laser Settings")]
     [SerializeField] private Transform laserSpawnPosition;
@@ -17,13 +18,28 @@ public class Player : MonoBehaviour
     private float _canFireTimer = 0;
 
     [Header("TripleShot PowerUp")]
-    public bool _isTripleShotEnabled = false; //Todo: Currently testing, eventually make this private
+    private bool _isTripleShotEnabled = false;
+    private IEnumerator _tripleShotTimerRef;
+
+    [Header("Speed PowerUp")]
+    [Tooltip("1.3f would be 30% faster")]
+    [SerializeField] private float speedPowerUpMultiplier = 1.3f;
+    private bool _isSpeedUpEnabled = false;
+    private IEnumerator _speedTimerRef;
 
     private SpawnManager _spawnManager;
+
+    public void Awake()
+    {
+        _speed = baseSpeed;
+    }
+
     public void Start()
     {
         _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
         if(_spawnManager == null){Debug.LogError("Spawn_Manager was null");}
+
+        
     }
 
     private void Update()
@@ -43,7 +59,8 @@ public class Player : MonoBehaviour
 
         var moveDirection = new Vector3(horizontalInput, verticalInput, 0).normalized;
 
-        transform.Translate((moveDirection * (speed * Time.deltaTime)));
+        transform.Translate((moveDirection * (_speed * Time.deltaTime)));    
+        
 
         #region Wrap And Clamp Screen Bounds
 
@@ -87,22 +104,62 @@ public class Player : MonoBehaviour
         Destroy(gameObject);
     }
 
-    private IEnumerator _powerUpTimerRef;
-    public void CollectPowerUp(PowerUp powerUp)
+    // private IEnumerator _powerUpTimerRef;
+    // public void CollectPowerUp(PowerUp powerUp)
+    // {
+    //     if (_powerUpTimerRef != null)
+    //     {
+    //         StopCoroutine(_powerUpTimerRef);
+    //     }
+    //     _isTripleShotEnabled = true; //TODO: unique PowerUp
+    //     _powerUpTimerRef = PowerUpTimer(powerUp.Duration);
+    //     StartCoroutine(_powerUpTimerRef);
+    // }
+    //
+    // private IEnumerator PowerUpTimer(float timer)
+    // {
+    //     Debug.Log("Timer");
+    //     yield return new WaitForSeconds(timer);
+    //     _isTripleShotEnabled = false;
+    // }
+    
+
+    public void CollectPowerUp_TripleShot(PowerUp powerUp)
     {
-        if (_powerUpTimerRef != null)
+        if (_tripleShotTimerRef != null)
         {
-            StopCoroutine(_powerUpTimerRef);
+            StopCoroutine(_tripleShotTimerRef);
         }
-        _isTripleShotEnabled = true; //TODO: unique PowerUp
-        _powerUpTimerRef = PowerUpTimer(powerUp.Duration);
-        StartCoroutine(_powerUpTimerRef);
+        _isTripleShotEnabled = true;
+        _tripleShotTimerRef = PowerUpTimer_TripleShot(powerUp.Duration);
+        StartCoroutine(_tripleShotTimerRef);
     }
 
-    private IEnumerator PowerUpTimer(float timer)
+    private IEnumerator PowerUpTimer_TripleShot(float timer)
     {
         Debug.Log("Timer");
         yield return new WaitForSeconds(timer);
         _isTripleShotEnabled = false;
+    }
+
+
+    public void CollectPowerUp_SpeedUp(PowerUp powerUp)
+    {
+        if (_speedTimerRef != null)
+        {
+            StopCoroutine(_speedTimerRef);
+        }
+        _isSpeedUpEnabled = true; 
+        _speed = baseSpeed * speedPowerUpMultiplier;
+        
+        _speedTimerRef = PowerUpTimer_SpeedUp(powerUp.Duration);
+        StartCoroutine(_speedTimerRef);
+    }
+
+    private IEnumerator PowerUpTimer_SpeedUp(float timer)
+    {
+        Debug.Log("Timer");
+        yield return new WaitForSeconds(timer);
+        _speed = baseSpeed;
     }
 }
--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -9,7 +9,9 @@ public class PowerUp : MonoBehaviour
 {
     [SerializeField] private float speed = 3.0f;
     [SerializeField] private float duration = 3.0f;
-
+    
+    [Tooltip("0 is tripleshot, 1 is speed, 2 is shield")]
+    [SerializeField] private int powerUpID;
     public float Duration
     {
         get => duration;
@@ -29,12 +31,24 @@ public class PowerUp : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
-        
         //Play Collection SFX
         var player = other.GetComponent<Player>();
         if (player)
         {
-            player.CollectPowerUp(this);
+            switch (powerUpID)
+            {
+                case 0:
+                    Debug.Log("Triple-shot Picked up");
+                    player.CollectPowerUp_TripleShot(this);
+                    break;
+                case 1:
+                    Debug.Log("Speed Picked up");
+                    player.CollectPowerUp_SpeedUp(this);
+                    break;
+                case 2:
+                    Debug.Log("Shield Picked up");
+                    break;
+            }
         }
         Destroy(gameObject);
     }
