bb0f0b87bb2691ab4432fd9aee9a8ccd529f8b82
Marc Hewitt <marc@marchewitt.com>
2021-05-12 13:30:07 -0500

    Setup OnEnemyDestroy Animation
    And did some cleanup as realized I was using Monobehavior OnDestroy by accident

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,24 +1,45 @@
 using System;
+using System.Collections;
 using Config;
+using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+
+[RequireComponent(typeof(Animator))]
 public class Enemy : MonoBehaviour
 {
+    private Animator _animatorRef;
     private Player _playerRef;
+    private BoxCollider2D _collider2DRef;
     [SerializeField] private int pointsWorth = 10;
     [Tooltip("Enemy's movement speed")]
-    [SerializeField] private float speed = 4.0f;
+    [SerializeField] private float baseSpeed = 4.0f;
+    private float _speed;
 
+    #region Animations
+    private static readonly int OnDestroyHash = Animator.StringToHash("OnDestroy"); //Animation Trigger
+    #endregion
+
+    private void Awake()
+    {
+        _animatorRef = gameObject.GetComponent<Animator>();
+        if(_animatorRef == null){Debug.LogError("_animatorRef was null in " + gameObject);}
+
+        _collider2DRef = gameObject.GetComponent<BoxCollider2D>();
+        if(_collider2DRef == null){Debug.LogError("_collider2DRef was null in " + gameObject);}
+    }
     private void Start()
     {
         _playerRef = GameObject.FindWithTag("Player").GetComponent<Player>();
         if(_playerRef == null){Debug.LogError("_playerRef was null in " + gameObject);}
+
+        _speed = baseSpeed;
     }
 
     private void Update()
     {
-        transform.Translate(Vector3.down * (speed * Time.deltaTime));
+        transform.Translate(Vector3.down * (_speed * Time.deltaTime));
 
         if (transform.position.y < ScreenBounds.ScreenBottom)
         {
@@ -27,17 +48,7 @@ public class Enemy : MonoBehaviour
         
     }
 
-    private void Respawn(float oldX)
-    {
-        var randomX = oldX;
-        while (oldX - 0.1f <= randomX && randomX <= oldX + 0.1f)
-        {
-            //Random spawn position until not close to old x position.
-            randomX = Random.Range(ScreenBounds.ScreenLeft, ScreenBounds.ScreenRight);
-        }
 
-        transform.position = new Vector3(randomX, ScreenBounds.ScreenTop + 2f, 0);
-    }
 
 
 
@@ -46,9 +57,9 @@ public class Enemy : MonoBehaviour
         //Debug.Log("hit");
         if (other.CompareTag("Player_Attack"))
         {
-            other.GetComponent<Laser>().DestroyUs();
+            other.GetComponent<Laser>().OnDestroyLaser();
             if( _playerRef ) { _playerRef.AddScore(pointsWorth); }
-            DestroyUs();
+            OnEnemyDestroy();
         }
         else if (other.CompareTag("Player"))
         {
@@ -61,13 +72,42 @@ public class Enemy : MonoBehaviour
             {
                 Debug.LogError("Player was null!!!");
             }
-            DestroyUs();
+            OnEnemyDestroy();
         }
     }
 
-    private void DestroyUs()
+    private void OnEnemyDestroy()
     {
-        //TODO: Instantiate(collisionPrefab, transform.position, transform.rotation);
+        StartCoroutine(OnEnemyDestroyDelay());
+        //Destroy(gameObject);
+        //Respawn(transform.position.x);
+    }
+
+    /// <summary>
+    /// Delaying for animation
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator OnEnemyDestroyDelay()
+    {
+        _collider2DRef.enabled = false;
+        _animatorRef.SetTrigger(OnDestroyHash);
+        var clipLength = 2.6333f;//Not working as intended: _animatorRef.GetCurrentAnimatorStateInfo((0)).length;
+        _speed = 0f;
+        yield return new WaitForSeconds(clipLength);
         Respawn(transform.position.x);
+    }
+    
+    private void Respawn(float oldX)
+    {
+        _collider2DRef.enabled = true;
+        _speed = baseSpeed;
+        var randomX = oldX;
+        while (oldX - 0.1f <= randomX && randomX <= oldX + 0.1f)
+        {
+            //Random spawn position until not close to old x position.
+            randomX = Random.Range(ScreenBounds.ScreenLeft, ScreenBounds.ScreenRight);
+        }
+
+        transform.position = new Vector3(randomX, ScreenBounds.ScreenTop + 3f, 0);
     }
 }
